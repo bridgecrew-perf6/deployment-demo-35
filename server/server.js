5446a1f4549c22599326a49e7d534305906ce118
Jamie Smith <thejamiemsmith@gmail.com>
2022-03-16 12:29:13 -0600

    adding middleware

--- server/server.js
+++ server/server.js
@@ -6,13 +6,19 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/index.html'))
-})
+//Middleware
 
-app.get("/styles", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.css"))
-})
+//.use means middleward
+app.use(express.static(path.join(__dirname, "../public")))
+
+//Endpoints
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname, '../public/index.html'))
+// })
+
+// app.get("/styles", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/index.css"))
+// })
 
 const port = process.env.PORT || 4005
 
